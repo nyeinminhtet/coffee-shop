ef39d50a64b6fe54d4c3abbd2de8703f66a459c1
nyeinminhtet <nyeinmg904@gmail.com>
2024-02-06 15:23:47 +0630

    init-pj-and-set-up-screens

---
+++ src/screens/OrderHistorScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const OrderHistorScreen = () => {
+  return (
+    <View>
+      <Text>OrderHistorScreen</Text>
+    </View>
+  );
+};
+
+export default OrderHistorScreen;
+
+const styles = StyleSheet.create({});
