2d04077922bef6073bcc88fc377899cb4d5e720a
nyeinminhtet <nyeinmg904@gmail.com>
2024-02-11 19:01:05 +0630

    add

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {useStore} from '../store/store';
+import {COLORS} from '../theme/theme';
 
-const DetailsScreen = () => {
+const DetailsScreen = ({navigation, route}: any) => {
+  const ItemofIndex = useStore((state: any) =>
+    route.params.type === 'Coffee' ? state.Coffee : state.BeanList,
+  )[route.params.index];
+
   return (
-    <View>
-      <Text>DetailsScreen</Text>
+    <View style={styles.ScreenContainer}>
+      <StatusBar backgroundColor={COLORS.primaryBlackHex} />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.ScrollViewFlex}></ScrollView>
     </View>
   );
 };
 
 export default DetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+  },
+  ScrollViewFlex: {
+    flexGrow: 1,
+  },
+});
